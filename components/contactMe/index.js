8e932b22f396ab3477e180394198a158a63cc010
Abel Rangel <ransilad@gmail.com>
2022-10-16 15:56:17 -0300

    Minor change

--- components/contactMe/index.js
+++ components/contactMe/index.js
@@ -1,16 +1,37 @@
 import styles from './contactMe.module.css'
+import Image from 'next/image'
 
 export default function ContactMe () {
   return (
-    <div className={styles.contactMeContainer}>
-      <div className="container">
-        <p className={styles.title}>¿Tiene un proyecto en mente?</p>
-        <p className={styles.description}>
-          Estoy disponible para proyectos freelance. Si tiene alguna pregunta o simplemente quiere decir hola, no dude
-          en contactarme.
-        </p>
-        <button>Contáctame</button>
+    <>
+      <div className={styles.contactMeContainerWithImage}>
+        <div className="container">
+          <div className={styles.titleAndSubtitle}>
+            <p className={styles.title}>Mantengámonos en contacto</p>
+            <p className={styles.subTitle}>Habla conmigo y trabajemos juntos</p>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className={styles.contactMeContainer}>
+        <div className="container">
+          <div className={styles.content}>
+            <div className={styles.card}>
+              <div className={styles.image}>
+                <Image src="/images/phone_illustrations.png" width={266} height={213} alt="" layout="responsive"/>
+              </div>
+              <p>Llámame</p>
+              <a href="tel:+56930627603" target="_blank" rel="noreferrer">+56930627603</a>
+            </div>
+            <div className={styles.card}>
+              <div className={styles.image}>
+                <Image src="/images/mail_illustrations.png" width={266} height={221} alt="" layout="responsive"/>
+              </div>
+              <p>Envíame un correo electrónico</p>
+              <a href="mailto:abeld.rangels@gmail.com" target="_blank" rel="noreferrer">abeld.rangels@gmail.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
---
+++ components/contactMeSimple/index.js
@@ -0,0 +1,21 @@
+import styles from './contactMeSimple.module.css'
+import Link from 'next/link'
+
+export default function ContactMeSimple () {
+  return (
+    <div className={styles.contactMeContainer}>
+      <div className="container">
+        <p className={styles.title}>¿Tiene un proyecto en mente?</p>
+        <p className={styles.description}>
+          Estoy disponible para proyectos freelance. Si tiene alguna pregunta o simplemente quiere decir hola, no dude
+          en contactarme.
+        </p>
+        <Link href={'/#contact-me'}>
+          <a className={styles.button}>
+            Contáctame
+          </a>
+        </Link>
+      </div>
+    </div>
+  )
+}
