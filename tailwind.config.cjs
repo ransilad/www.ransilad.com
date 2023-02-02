c900c03687312b3acab5abdc23519be7aeebee42
Abel Rangel <abeld.rangels@gmail.com>
2023-02-02 00:58:28 -0300

    Migrate to Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+			sm: '590px',
+			md: '640px',
+			lg: '768px',
+			xl: '1600px',
+		},
+		extend: {
+			colors: {
+        alabaster: '#F8F8F8',
+        concrete: '#F3F3F3',
+				primary: '#49be7e',
+				emperor: '#515151',
+      }
+		},
+	},
+	plugins: [],
+}
