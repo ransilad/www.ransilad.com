e055bf7247e82b263024ceacb6a1792eaf028c6c
Abel Rangel <abeld.rangels@gmail.com>
2024-08-09 16:39:22 -0400

    Add new style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-			sm: '590px',
-			md: '640px',
-			lg: '1025px',
-			xl: '1600px',
-		},
-		extend: {
-			colors: {
-				concrete: '#F3F3F3',
-				primary: '#00893e',
-				emperor: '#515151',
-			},
-			backgroundImage: {
-				'contact-me-banner': "url('/images/contact-me-banner.webp')",
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      sm: '590px',
+      md: '640px',
+      lg: '1025px',
+      xl: '1600px'
+    },
+    extend: {
+      colors: {
+        concrete: '#F3F3F3',
+        primary: '#00ca5b',
+        emperor: '#515151'
+      },
+      backgroundImage: {
+        'contact-me-banner': "url('/images/contact-me-banner.webp')"
+      }
+    }
+  },
+  plugins: []
 }
