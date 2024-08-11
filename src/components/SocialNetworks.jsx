126403d28b7a38a50809154fe94dbff25c083f14
Abel Rangel <abeld.rangels@gmail.com>
2024-08-10 20:12:22 -0400

    Adding tooltip with react integration

---
+++ src/components/SocialNetworks.jsx
@@ -0,0 +1,55 @@
+import { Tooltip } from 'react-tooltip'
+
+const SocialNetworks = () => {
+  return (
+    <div class='mt-6 flex'>
+      <a
+        class='p-2 rounded-lg transition duration-200 hover:bg-zinc-800 hover:text-accent-foreground/80'
+        href='https://www.linkedin.com/in/ransilad'
+        target='_blank'
+        rel='noreferrer'
+        aria-label='LinkedIn'
+        data-tooltip-id='rrss-tooltip'
+        data-tooltip-content='LinkedIn'
+      >
+        <img src='/icons/linkedIn.svg' alt='LinkedIn' width='28' height='28' />
+      </a>
+      <a
+        class='p-2 rounded-lg transition duration-200 hover:bg-zinc-800 hover:text-accent-foreground/80'
+        href='https://github.com/ransilad'
+        target='_blank'
+        rel='noreferrer'
+        aria-label='Github'
+        data-tooltip-id='rrss-tooltip'
+        data-tooltip-content='Github'
+      >
+        <img src='/icons/github.svg' alt='Github' width='28' height='28' />
+      </a>
+      <a
+        class='p-2 rounded-lg transition duration-200 hover:bg-zinc-800 hover:text-accent-foreground/80'
+        href='https://twitter.com/ransilad'
+        target='_blank'
+        rel='noreferrer'
+        aria-label='X (Twitter)'
+        data-tooltip-id='rrss-tooltip'
+        data-tooltip-content='X (Twitter)'
+      >
+        <img src='/icons/twitter.svg' alt='X (Twitter)' width='28' height='28' />
+      </a>
+      <a
+        class='p-2 rounded-lg transition duration-200 hover:bg-zinc-800 hover:text-accent-foreground/80'
+        href='https://drive.google.com/file/d/1Q3vHdCzExbZzoXyZ0RRChbyNi9R1pm40/view?usp=sharing'
+        target='_blank'
+        rel='noreferrer'
+        aria-label='Curriculum'
+        data-tooltip-id='rrss-tooltip'
+        data-tooltip-content='Currículum'
+      >
+        <img src='/icons/curriculum.svg' alt='Currículum' width='28' height='28' />
+      </a>
+      <Tooltip id='rrss-tooltip' place='bottom' type='dark' />
+    </div>
+  )
+}
+
+export default SocialNetworks
