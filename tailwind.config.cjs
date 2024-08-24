6dfb628e5a50d19c1458f7fee34563570665309c
Abel Rangel <abeld.rangels@gmail.com>
2024-08-24 13:55:45 -0400

    Enhance header and landing animations: introduce fade-in effects for smoother component transitions in the UI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,10 +23,20 @@ module.exports = {
           '0%': { translate: '0 0' },
           '50%': { translate: '0 -15px' },
           '100%': { translate: '0 0' }
+        },
+        'opacity-fade-in': {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        'opacity-translate-y-fade-in': {
+          '0%': { opacity: 0, transform: 'translateY(-10px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' }
         }
       },
       animation: {
-        float: 'float 3s ease-in-out infinite alternate'
+        float: 'float 3s ease-in-out infinite alternate',
+        'opacity-fade-in': 'opacity-fade-in 1s var(--animation-delay, 0ms) ease forwards',
+        'opacity-translate-y-fade-in': 'opacity-translate-y-fade-in 1s var(--animation-delay, 0ms) ease forwards'
       },
       boxShadow: {
         '3xl-center': '0 0px 104px -40px'
