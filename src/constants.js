3aef23240013cb884b4f845ca03d785a95bbfe60
Abel Rangel <abeld.rangels@gmail.com>
2024-11-27 14:40:10 -0300

    Add constants for portfolio and work items, enhancing structure for managing project data and assets in the application

---
+++ src/constants.js
@@ -0,0 +1,282 @@
+export const PORTFOLIO_ITEMS = [
+  {
+    id: 'inlet',
+    title: 'Inlet',
+    image: import('@assets/images/portfolio/inlet.avif'),
+    tags: ['ReactJS', 'React Native', 'JavaScript', 'HTML/CSS', 'AWS', 'Gitlab', 'Google Analytics', 'Google Tag Manager'],
+    description: 'Plataforma que facilita la conexión entre comunidades.',
+    isTop: true
+  },
+  {
+    id: 'voyhoy',
+    title: 'Voyhoy',
+    image: import('@assets/images/portfolio/voyhoy.avif'),
+    tags: ['AngularJS', 'JavaScript', 'HTML/CSS', 'Python', 'PostgreSQL', 'AWS', 'Github', 'Adyen', 'PayU', 'Paypal', 'Voucherify', 'Optimizely'],
+    description:
+      'Plataforma que permite a los usuarios planificar sus viajes, incluyendo la posibilidad de ' +
+      'conexiones entre diferentes tipos de transporte, lo que denominamos Smart Ticket.',
+    isTop: true
+  },
+  {
+    id: 'damath',
+    title: 'Damath',
+    image: import('@assets/images/portfolio/damath.avif'),
+    tags: ['Design', 'HTML/CSS', 'Wordpress'],
+    description: 'Sitio web para gestionar ventas de productos cortados con láser.'
+  },
+  {
+    id: 'techk',
+    title: 'techk',
+    image: import('@assets/images/portfolio/techk.avif'),
+    url: 'https://www.instoreview.com/inicio',
+    tags: ['Next.js', 'JavaScript', 'HTML/CSS', 'Python', 'Docker'],
+    description: 'Plataforma que permite clasificar los productos que entran a las tiendas.'
+  },
+  {
+    id: 'cocha',
+    title: 'Cocha',
+    image: import('@assets/images/portfolio/cocha.avif'),
+    url: 'https://www.cocha.com/',
+    tags: ['Angular 7', 'JavaScript', 'Bootstrap', 'HTML/CSS', 'NodeJS', 'Bitbucket', 'MySql'],
+    description: 'Sistema web que ayudar a los vendedores a gestionar los pasajes de sus clientes.'
+  },
+  {
+    id: 'tendy',
+    title: 'Tendy',
+    image: import('@assets/images/portfolio/tendy.avif'),
+    url: 'https://tendy.cash/',
+    tags: ['VueJS', 'React Native', 'JavaScript', 'HTML/CSS', 'NodeJS', 'AWS', 'Github'],
+    description: 'Plataforma que permite “Otorgar propinas de una forma fácil y anónima”.',
+    isTop: true
+  },
+  {
+    id: 'idc',
+    title: 'IDC',
+    image: import('@assets/images/portfolio/idc.avif'),
+    tags: ['ReactJS', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'AWS', 'Github'],
+    description: 'Sistema de gestión de contenido y educación.'
+  },
+  {
+    id: 'a21',
+    title: 'A21',
+    image: import('@assets/images/portfolio/a21.avif'),
+    url: 'https://www.antarctica21.com/',
+    tags: ['React Native', 'JavaScript', 'HTML/CSS', 'Redux'],
+    description: 'Aplicación móvil que permite gestionar toda la experiencia del viaje, desde la reserva hasta el check-out'
+  },
+  {
+    id: 'dtemite',
+    title: 'Dtemite',
+    image: import('@assets/images/portfolio/dtemite.avif'),
+    url: 'https://www.dtemite.cl/',
+    tags: ['Python', 'Flask', 'React Native', 'JavaScript', 'HTML/CSS', 'Redux', 'Bitbucket', 'Redis'],
+    description: 'ERP que centraliza la información referente al SII en un solo sitio.',
+    isTop: true
+  },
+  {
+    id: 'nrp',
+    title: 'NextRoundPartners',
+    image: import('@assets/images/portfolio/nrp.avif'),
+    url: 'https://www.nextroundpartners.com/',
+    tags: ['ReactJS', 'JavaScript', 'Design', 'HTML/CSS', 'Bootstrap'],
+    description: 'Sitio web para la empresa NextRoundPartners.'
+  },
+  {
+    id: 'ona',
+    title: 'OnaStudio',
+    image: import('@assets/images/portfolio/ona.avif'),
+    url: 'https://www-onastudio-co.vercel.app/',
+    tags: ['Next.js', 'JavaScript', 'Design', 'HTML/CSS'],
+    description: 'Sitio web para la empresa OnaStudio.'
+  },
+  {
+    id: 'lopesa',
+    title: 'Lopesa',
+    image: import('@assets/images/portfolio/lopesa.avif'),
+    url: 'http://sazonlopesa.com/aplicativo2',
+    tags: ['Angular 12', 'JavaScript', 'HTML/CSS', 'Ionic', 'Bootstrap', 'Github'],
+    description: 'Plataforma que permite gestionar productos.'
+  },
+  {
+    id: 'svp-condominio',
+    isPersonal: true,
+    title: 'SVP Condominio',
+    image: import('@assets/images/portfolio/svp-condominio.avif'),
+    url: 'https://condominio-svp.vercel.app/',
+    tags: ['ReactJS', 'JavaScript', 'HTML/CSS', 'Bootstrap', 'Github'],
+    description: 'Plataforma que permite gestionar toda la información de su condominio.',
+    githubUrl: 'https://github.com/ransilad/condominio-svp'
+  },
+  {
+    id: 'arrows-sports',
+    title: 'Arrows Santiago Sports',
+    image: import('@assets/images/portfolio/arrows-sports.avif'),
+    url: 'https://arrows.santiagosport.cl/',
+    tags: ['Next.js', 'HTML/CSS', 'JavaScript', 'Github', 'TailwindCSS'],
+    description: 'Sitio web para la empresa Arrows Santiago Sports.'
+  },
+  {
+    id: 'blog-arrows-sports',
+    title: 'Blog Arrows Santiago Sports',
+    image: import('@assets/images/portfolio/blog-arrows-sports.avif'),
+    url: 'https://blog.arrows.santiagosport.cl/',
+    tags: ['Astro', 'HTML/CSS', 'JavaScript', 'Github', 'TailwindCSS'],
+    description: 'Blog para la empresa Arrows Santiago Sports.',
+    isTop: true
+  },
+  {
+    id: 'games-ransilad',
+    isPersonal: true,
+    title: 'Juegos',
+    image: import('@assets/images/portfolio/games-ransilad.avif'),
+    url: 'https://games.ransilad.com/',
+    tags: ['Astro', 'HTML/CSS', 'JavaScript', 'Github', 'TailwindCSS'],
+    description: 'Página web que junta diferentes juegos creados por mi (Abel Rangel).',
+    githubUrl: 'https://github.com/ransilad/games.ransildad.com',
+    isTop: true
+  },
+  {
+    id: 'tres-en-raya-ransilad',
+    isPersonal: true,
+    title: 'Juego Tres en Raya',
+    image: import('@assets/images/portfolio/tres-en-raya-ransilad.avif'),
+    url: 'https://tres-en-raya.ransilad.com/',
+    tags: ['ReactJS', 'HTML/CSS', 'JavaScript', 'Github', 'TailwindCSS', 'Vite', 'Firebase', 'Realtime Database'],
+    description: 'Juego de tres en raya, el cual dispone de 3 modos de juego: local, contra el bot (fácil o difícil) y multijugador.',
+    githubUrl: 'https://github.com/ransilad/tres-en-raya.ransilad.com'
+  },
+  {
+    id: 'syn-ant-definition-finder',
+    isPersonal: true,
+    title: 'Buscador de sinónimos, antónimos y definiciones',
+    image: import('@assets/images/portfolio/syn-ant-definition-finder.avif'),
+    url: 'https://syn-ant-definition-finder.ransilad.com/',
+    tags: ['Next.js', 'JavaScript', 'Design', 'HTML/CSS', 'OpenAI'],
+    description: 'Buscador de sinónimos, antónimos y definiciones, el cual hace uso de la API de OpenAI.',
+    githubUrl: 'https://github.com/ransilad/syn-ant-definition-finder'
+  },
+  {
+    id: 'memory-game',
+    isPersonal: true,
+    title: 'Memoría',
+    image: import('@assets/images/portfolio/memory-game.avif'),
+    url: 'https://memory.ransilad.com/',
+    tags: ['React', 'JavaScript', 'Design', 'HTML/CSS', 'Vercel'],
+    description: 'En este juego, el jugador debe ingresar su nombre, luego levantar una carta y buscar su pareja hasta completar el tablero.',
+    githubUrl: 'https://github.com/ransilad/memory-game'
+  },
+  {
+    id: 'esemed',
+    isPersonal: false,
+    title: 'ESEMED',
+    image: import('@assets/images/portfolio/esemed.avif'),
+    url: 'https://www.esemed.cl/',
+    tags: ['Next.js', 'JavaScript', 'Design', 'HTML/CSS'],
+    description: 'ESEMED, es una landing page que permite a los usuarios conocer los productos de Innovación médica estética.'
+  }
+]
+
+export const WORK_KUBOS_ITEMS = [
+  {
+    title: 'Angular',
+    imagen: import('@assets/icons/brands/angular.svg')
+  },
+  {
+    title: 'React Native',
+    imagen: import('@assets/icons/brands/react-native.svg')
+  },
+  {
+    title: 'Git',
+    imagen: import('@assets/icons/brands/git.svg')
+  },
+  {
+    title: 'Jenkins',
+    imagen: import('@assets/icons/brands/jenkins.svg')
+  },
+  {
+    title: 'Docker',
+    imagen: import('@assets/icons/brands/docker.svg')
+  },
+  {
+    title: 'Kubernetes',
+    imagen: import('@assets/icons/brands/kubernetes.svg')
+  },
+  {
+    title: 'Terraform',
+    imagen: import('@assets/icons/brands/terraform.svg')
+  },
+  {
+    title: 'Python',
+    imagen: import('@assets/icons/brands/python.svg')
+  }
+]
+
+export const WORK_ONA_ITEMS = [
+  {
+    title: 'Freelance',
+    imagen: import('@assets/icons/brands/freelance.svg')
+  },
+  {
+    title: 'React',
+    imagen: import('@assets/icons/brands/react-native.svg')
+  },
+  {
+    title: 'React Native',
+    imagen: import('@assets/icons/brands/react-native.svg')
+  },
+  {
+    title: 'Git',
+    imagen: import('@assets/icons/brands/git.svg')
+  },
+  {
+    title: 'Astro',
+    imagen: import('@assets/icons/brands/astro.svg')
+  },
+  {
+    title: 'TailwindCSS',
+    imagen: import('@assets/icons/brands/tailwind.svg')
+  },
+  {
+    title: 'Github Actions',
+    imagen: import('@assets/icons/brands/git.svg')
+  }
+]
+
+export const WORK_DTEMITE_ITEMS = [
+  {
+    title: 'Freelance',
+    imagen: import('@assets/icons/brands/freelance.svg')
+  },
+  {
+    title: 'React',
+    imagen: import('@assets/icons/brands/react-native.svg')
+  },
+  {
+    title: 'React Native',
+    imagen: import('@assets/icons/brands/react-native.svg')
+  },
+  {
+    title: 'Astro',
+    imagen: import('@assets/icons/brands/astro.svg')
+  },
+  {
+    title: 'NextJS',
+    imagen: import('@assets/icons/brands/nextjs.svg')
+  },
+  {
+    title: 'TailwindCSS',
+    imagen: import('@assets/icons/brands/tailwind.svg')
+  },
+  {
+    title: 'Python',
+    imagen: import('@assets/icons/brands/python.svg')
+  },
+  {
+    title: 'Git',
+    imagen: import('@assets/icons/brands/git.svg')
+  },
+  {
+    title: 'Github Actions',
+    imagen: import('@assets/icons/brands/git.svg')
+  }
+]
